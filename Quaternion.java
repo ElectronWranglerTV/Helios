6a3bf621cf7ceb6f9d71dfdf0bf2595446348f1c
ElectronWrangler <72363575+ElectronWranglerTV@users.noreply.github.com>
2020-12-29 15:42:03 -0800

    Update Quaternion.java

--- Quaternion.java
+++ Quaternion.java
@@ -1,3 +1,9 @@
+;* Helios (TM) 3D Engine (Java): Quaternion Library
+;* Copyright (C) DeRemee Systems, IXE Electronics LLC
+;* Portions copyright IXE Electronics LLC, Republic Robotics, FemtoLaunch, FemtoSat, FemtoTrack, Weland
+;* This work is made available under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+;* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+
 package Math;
 
 public class Quaternion {
